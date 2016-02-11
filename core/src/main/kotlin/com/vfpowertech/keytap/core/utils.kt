70f6a52c69bc2f4ecd25e2534c59a8e33f05db5a
Kevin Tardif <kevin@vfpowertech.com>
2016-02-11 14:43:27 -0500

    Added some core utility functions

--- core/src/main/kotlin/com/vfpowertech/keytap/core/utils.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/utils.kt
@@ -1,6 +1,9 @@
+@file:JvmName("CoreUtils")
 package com.vfpowertech.keytap.core
 
 import com.fasterxml.jackson.core.type.TypeReference
+import java.io.File
+import java.io.FileOutputStream
 
 /**
  * Throw an IllegalArgumentException with the given message if the predicate is false.
@@ -12,5 +15,45 @@ inline fun require(predicate: Boolean, message: String) {
         throw IllegalArgumentException(message)
 }
 
-/** Shorthand for creating Jackson TypeRefrences */
+/** Shorthand for creating Jackson TypeRefrences. */
 inline fun <reified T> typeRef(): TypeReference<T> = object : TypeReference<T>() {}
+
+/** Read entire resource file as a UTF-8 text file. */
+fun Class<*>.readResourceFileText(path: String): String =
+    getResourceAsStream(path).bufferedReader().use {
+        it.readText()
+    }
+
+/** Get the expected platform file name for a shared library. */
+fun getSharedLibFileName(base: String): String {
+    val os = System.getProperty("os.name")
+    return when {
+        os == "Linux" -> "lib${base}.so"
+        os.startsWith("Windows") -> "${base}.dll"
+    //TODO osx
+        else -> throw UnsupportedOperationException("Unsupported OS: $os")
+    }
+}
+
+//this is in core to simplify testing; should only be used by the desktop port
+/** Attempts to unpack and load a shared library from resources. Do not use on android. */
+fun Class<*>.loadSharedLibFromResource(base: String) {
+    val platformName = getSharedLibFileName(base)
+
+    val inputStream = getResourceAsStream("/$platformName")
+    if (inputStream == null)
+        throw UnsatisfiedLinkError("Unable to find shared library $platformName")
+
+    val path = File.createTempFile("sqlitetest", "")
+    path.deleteOnExit()
+    //according to the JNA src code, ext .dll is required for loading on windows
+    inputStream.use { sharedLibStream ->
+        FileOutputStream(path).use {
+            val buffer = ByteArray(4096)
+            while (sharedLibStream.read(buffer) > 0)
+                it.write(buffer)
+        }
+    }
+
+    System.load(path.toString())
+}
