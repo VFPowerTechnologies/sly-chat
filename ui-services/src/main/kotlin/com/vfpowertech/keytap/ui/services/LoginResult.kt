93687d1b2cab47e0045291b6ba2072709bde8b0c
Kevin Tardif <kevin@vfpowertech.com>
2016-02-12 09:51:13 -0500

    Added LoginResult to LoginService.login

---
+++ ui-services/src/main/kotlin/com/vfpowertech/keytap/ui/services/LoginResult.kt
@@ -0,0 +1,3 @@
+package com.vfpowertech.keytap.ui.services
+
+data class LoginResult(val successful: Boolean, val errorMessage: String?)
--- ui-services/src/main/kotlin/com/vfpowertech/keytap/ui/services/dummy/LoginServiceImpl.kt
+++ ui-services/src/main/kotlin/com/vfpowertech/keytap/ui/services/dummy/LoginServiceImpl.kt
@@ -1,10 +1,11 @@
 package com.vfpowertech.keytap.ui.services.dummy
 
+import com.vfpowertech.keytap.ui.services.LoginResult
 import com.vfpowertech.keytap.ui.services.LoginService
 import nl.komponents.kovenant.Promise
 
 class LoginServiceImpl : LoginService {
-    override fun login(emailOrPhoneNumber: String, password: String): Promise<Unit, Exception> {
-        return Promise.ofSuccess(Unit)
+    override fun login(emailOrPhoneNumber: String, password: String): Promise<LoginResult, Exception> {
+        return Promise.ofSuccess(LoginResult(true, null))
     }
 }
