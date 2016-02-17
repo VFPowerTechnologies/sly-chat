ef1decf76351ca5983c6732fcc23739eabd55d22
Kevin Tardif <kevin@vfpowertech.com>
2016-02-17 11:19:37 -0500

    Added Contacts/PreKeyPersistenceManagers to DI

--- ui-services/src/main/kotlin/com/vfpowertech/keytap/ui/services/di/PersistenceModule.kt
+++ ui-services/src/main/kotlin/com/vfpowertech/keytap/ui/services/di/PersistenceModule.kt
@@ -2,7 +2,12 @@ package com.vfpowertech.keytap.ui.services.di
 
 import com.vfpowertech.keytap.core.PlatformInfo
 import com.vfpowertech.keytap.core.persistence.AccountInfoPersistenceManager
+import com.vfpowertech.keytap.core.persistence.ContactsPersistenceManager
 import com.vfpowertech.keytap.core.persistence.JsonAccountInfoPersistenceManager
+import com.vfpowertech.keytap.core.persistence.PreKeyPersistenceManager
+import com.vfpowertech.keytap.core.persistence.sqlite.SQLiteContactsPersistenceManager
+import com.vfpowertech.keytap.core.persistence.sqlite.SQLitePersistenceManager
+import com.vfpowertech.keytap.core.persistence.sqlite.SQLitePreKeyPersistenceManager
 import dagger.Module
 import dagger.Provides
 import java.io.File
@@ -14,4 +19,22 @@ class PersistenceModule {
     @Provides
     fun providesAccountInfoPersistenceManager(platformInfo: PlatformInfo): AccountInfoPersistenceManager =
         JsonAccountInfoPersistenceManager(File(platformInfo.dataFileStorageDirectory, "account-info.json"))
+
+    @Singleton
+    @Provides
+    fun providesPreKeyPersistenceManager(sqlitePersistenceManager: SQLitePersistenceManager): PreKeyPersistenceManager =
+        SQLitePreKeyPersistenceManager(sqlitePersistenceManager)
+
+    @Singleton
+    @Provides
+    fun providesContactsPersistenceManager(sqlitePersistenceManager: SQLitePersistenceManager): ContactsPersistenceManager =
+        SQLiteContactsPersistenceManager(sqlitePersistenceManager)
+
+    //TODO I'm an idiot, need to scope this
+    @Singleton
+    @Provides
+    fun providesSQLitePersistenceManager(platformInfo: PlatformInfo): SQLitePersistenceManager {
+        val path = File(platformInfo.dataFileStorageDirectory, "db.sqlite3")
+        return SQLitePersistenceManager(path, ByteArray(0), null)
+    }
 }
