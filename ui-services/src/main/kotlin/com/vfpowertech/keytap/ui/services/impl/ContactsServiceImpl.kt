9d08e408175b4e8c98807c17a55d3b3378abeab7
guillaume <guillaume.reeves@gmail.com>
2016-02-22 12:50:58 -0500

    fixed ContactServiceImpl

--- ui-services/src/main/kotlin/com/vfpowertech/keytap/ui/services/impl/ContactsServiceImpl.kt
+++ ui-services/src/main/kotlin/com/vfpowertech/keytap/ui/services/impl/ContactsServiceImpl.kt
@@ -4,64 +4,35 @@ import com.vfpowertech.keytap.core.persistence.ContactsPersistenceManager
 import com.vfpowertech.keytap.core.persistence.ContactInfo
 import com.vfpowertech.keytap.ui.services.ContactsService
 import com.vfpowertech.keytap.ui.services.UIContactDetails
-import com.vfpowertech.keytap.ui.services.dummy.InvalidContactException
 import nl.komponents.kovenant.Promise
-import nl.komponents.kovenant.functional.bind
-import java.util.HashMap
+import nl.komponents.kovenant.functional.map
+import nl.komponents.kovenant.ui.successUi
 
 class ContactsServiceImpl(
     private val contactsPersistenceManager: ContactsPersistenceManager
 ) : ContactsService {
 
-    private var contacts = HashMap<Int, UIContactDetails>()
-
-    private fun fetchContacts() {
-        contacts.clear()
-
-        val contactList = contactsPersistenceManager.getAll().get()
-
-        for(i in contactList.indices){
-            contacts.put(i, UIContactDetails(i, contactList[i].name, contactList[i].phoneNumber, contactList[i].email, contactList[i].publicKey))
-        }
-    }
-
     override fun updateContact(newContactDetails: UIContactDetails) {
-        if (newContactDetails.id == null)
-            throw IllegalArgumentException("Contact id was null")
-
-        contacts[newContactDetails.id] ?: throw InvalidContactException(newContactDetails)
-
         contactsPersistenceManager.update(ContactInfo(newContactDetails.email, newContactDetails.name, newContactDetails.phoneNumber, newContactDetails.publicKey))
-        contacts[newContactDetails.id] = newContactDetails
     }
 
     override fun getContacts(): Promise<List<UIContactDetails>, Exception> {
-        fetchContacts()
-
-        return Promise.ofSuccess<List<UIContactDetails>, Exception>(contacts.values.toList())
+        return contactsPersistenceManager.getAll() map { contacts ->
+            contacts.map { c -> UIContactDetails(c.name, c.phoneNumber, c.email, c.publicKey) }
+        } successUi { uiContacts ->
+            Promise.ofSuccess<List<UIContactDetails>, Exception>(uiContacts)
+        }
     }
 
     override fun addNewContact(contactDetails: UIContactDetails): Promise<UIContactDetails, Exception>{
-        if (contactDetails.id != null)
-            throw IllegalArgumentException("Contact id was not null")
-
-        return contactsPersistenceManager.add(ContactInfo(contactDetails.email, contactDetails.name, contactDetails.phoneNumber, contactDetails.publicKey)) bind {
-            val id = contacts.size
-            val withId = contactDetails.copy(id = id)
-            contacts.put(id, contactDetails)
-            Promise.ofSuccess<UIContactDetails, Exception>(withId)
+        return contactsPersistenceManager.add(ContactInfo(contactDetails.email, contactDetails.name, contactDetails.phoneNumber, contactDetails.publicKey)) map {
+            contactDetails
+        } successUi { contactDetails ->
+            Promise.ofSuccess<UIContactDetails, Exception>(contactDetails)
         }
     }
 
     override fun removeContact(contactDetails: UIContactDetails): Promise<Unit, Exception> {
-        if (contactDetails.id == null)
-            throw IllegalArgumentException("Contact id was null")
-
-        contacts[contactDetails.id] ?: throw InvalidContactException(contactDetails)
-
-        return contactsPersistenceManager.remove(ContactInfo(contactDetails.email, contactDetails.name, contactDetails.phoneNumber, contactDetails.publicKey)) bind {
-            fetchContacts()
-            Promise.ofSuccess<Unit, Exception>(Unit)
-        }
+        return contactsPersistenceManager.remove(ContactInfo(contactDetails.email, contactDetails.name, contactDetails.phoneNumber, contactDetails.publicKey))
     }
 }
