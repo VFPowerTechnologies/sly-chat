77000344d59e1adb5cfce10ee6abc8cc4e0f2a99
Kevin Tardif <kevin@vfpowertech.com>
2016-02-23 10:20:15 -0500

    Added UserScope

---
+++ ui-services/src/main/kotlin/com/vfpowertech/keytap/ui/services/di/UserScope.kt
@@ -0,0 +1,9 @@
+package com.vfpowertech.keytap.ui.services.di
+
+import javax.inject.Scope
+
+/** Marks a user login scope. */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class UserScope
+
