a6b25edfa25e22ea299962a6f7a3ab521829a890
Kevin Tardif <kevin@vfpowertech.com>
2016-03-04 10:02:30 -0500

    Removed usage of SecretKeySpec
    - we don't use the JCA interface anymore anyways

--- core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/SessionData.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/SessionData.kt
@@ -1,17 +1,15 @@
 package com.vfpowertech.keytap.core.persistence
 
+import com.vfpowertech.keytap.core.crypto.EncryptionSpec
 import com.vfpowertech.keytap.core.crypto.ciphers.CipherParams
 import com.vfpowertech.keytap.core.crypto.encryptDataWithParams
 import com.vfpowertech.keytap.core.crypto.hexify
-import javax.crypto.spec.SecretKeySpec
 
 data class SessionData(
     val authToken: String
 ) {
     fun serialize(localDataEncryptionKey: ByteArray, localDataEncryptionParams: CipherParams): SerializedSessionData {
-        val key = SecretKeySpec(localDataEncryptionKey, localDataEncryptionParams.keyType)
-
-        val encryptedAuthToken = encryptDataWithParams(key, authToken.toByteArray(Charsets.UTF_8), localDataEncryptionParams)
+        val encryptedAuthToken = encryptDataWithParams(EncryptionSpec(localDataEncryptionKey, localDataEncryptionParams), authToken.toByteArray(Charsets.UTF_8))
         return SerializedSessionData(encryptedAuthToken.data.hexify())
     }
 }
