52ea4941a37c31f94f5152407683308f38ab350f
Kevin Tardif <kevin@vfpowertech.com>
2016-03-04 11:07:08 -0500

    Removed JsonInclude on StartupInfo

--- core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/StartupInfo.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/StartupInfo.kt
@@ -1,10 +1,8 @@
 package com.vfpowertech.keytap.core.persistence
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
 /** Optional info used by the app on startup. */
-@JsonInclude(JsonInclude.Include.NON_NULL)
 data class StartupInfo(
     @JsonProperty("lastLoggedInAccount")
     val lastLoggedInAccount: String,
