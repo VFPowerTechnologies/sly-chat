82dce99a8677e80c85cbb06fe077009317686890
Kevin Tardif <kevin@vfpowertech.com>
2016-03-04 09:51:10 -0500

    JsonAccountInfoPersistenceManager no longer caching info

--- core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/JsonAccountInfoPersistenceManager.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/JsonAccountInfoPersistenceManager.kt
@@ -6,42 +6,22 @@ import nl.komponents.kovenant.task
 import java.io.File
 
 class JsonAccountInfoPersistenceManager(private val path: File) : AccountInfoPersistenceManager {
-    //make sure to serialize access to this
-    private var accountInfo: AccountInfo? = null
-
-    override fun retrieve(): Promise<AccountInfo?, Exception> {
-        synchronized(this) {
-            if (accountInfo != null)
-                return Promise.of(accountInfo)
-        }
-
-        return task {
-            if (!path.exists())
+    override fun retrieve(): Promise<AccountInfo?, Exception> = task {
+        if (!path.exists())
+            null
+        else {
+            val objectMapper = ObjectMapper()
+            val json = path.readText()
+            if (json.length == 0)
                 null
-            else {
-                val objectMapper = ObjectMapper()
-                val json = path.readText()
-                if (json.length == 0)
-                    null
-                else
-                    objectMapper.readValue(json, AccountInfo::class.java)
-            }
+            else
+                objectMapper.readValue(json, AccountInfo::class.java)
         }
     }
 
-    override fun store(accountInfo: AccountInfo): Promise<Unit, Exception> {
-        synchronized(this) {
-            if (accountInfo == this.accountInfo)
-                return Promise.ofSuccess(Unit)
-        }
-
-        return task {
-            val objectMapper = ObjectMapper()
-            val bytes = objectMapper.writeValueAsBytes(accountInfo)
-            path.outputStream().write(bytes)
-            synchronized(this) {
-                this.accountInfo = accountInfo
-            }
-        }
+    override fun store(accountInfo: AccountInfo): Promise<Unit, Exception> = task {
+        val objectMapper = ObjectMapper()
+        val bytes = objectMapper.writeValueAsBytes(accountInfo)
+        path.outputStream().write(bytes)
     }
 }
