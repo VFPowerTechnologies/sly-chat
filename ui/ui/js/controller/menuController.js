e199d0605d59d82433d1ef3329d8bf43089528a7
guillaume <guillaume.reeves@gmail.com>
2016-03-15 10:30:18 -0400

    Removed fake message button

--- ui/ui/js/controller/menuController.js
+++ ui/ui/js/controller/menuController.js
@@ -12,21 +12,6 @@ MenuController.prototype = {
             KEYTAP.loginController.logout();
         });
 
-        //Send a fake message to test receive message listener.
-        $(document).on("click", '#sendFakeMessage', function(e){
-            e.preventDefault();
-
-            var conversations = KEYTAP.contactController.getConversations();
-            for(email in conversations) {
-                if(conversations.hasOwnProperty(email)) {
-                    develService.receiveFakeMessage(conversations[email].contact, "fake message to: " + conversations[email].contact.name).catch(function (e) {
-                        KEYTAP.exceptionController.displayDebugMessage(e);
-                        console.log('receiveFakeMessage failed: ' + e);
-                    });
-                }
-            }
-        });
-
         $(document).on("click", "#addContactBtn", function (e) {
             e.preventDefault();
             KEYTAP.navigationController.loadPage("addContact.html");
