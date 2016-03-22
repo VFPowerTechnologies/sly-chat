de9337f673fb64037c960987fddf9b63fab8b82b
Kevin Tardif <kevin@vfpowertech.com>
2016-03-22 15:02:33 -0400

    Added readObjectFromJsonFile and writeObjectFromJsonFile

--- core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/json/JsonStartupInfoPersistenceManager.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/json/JsonStartupInfoPersistenceManager.kt
@@ -1,32 +1,17 @@
 package com.vfpowertech.keytap.core.persistence.json
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.vfpowertech.keytap.core.persistence.StartupInfo
 import com.vfpowertech.keytap.core.persistence.StartupInfoPersistenceManager
 import nl.komponents.kovenant.Promise
 import nl.komponents.kovenant.task
 import java.io.File
-import java.io.FileNotFoundException
 
 class JsonStartupInfoPersistenceManager(val path: File) : StartupInfoPersistenceManager {
     override fun store(startupInfo: StartupInfo): Promise<Unit, Exception> = task {
-        val objectMapper = ObjectMapper()
-        val bytes = objectMapper.writeValueAsBytes(startupInfo)
-        path.writeBytes(bytes)
+        writeObjectToJsonFile(path, startupInfo)
     }
 
     override fun retrieve(): Promise<StartupInfo?, Exception> = task {
-        try {
-            val bytes = path.readBytes()
-            if (bytes.isEmpty())
-                null
-            else {
-                val objectMapper = ObjectMapper()
-                objectMapper.readValue(bytes, StartupInfo::class.java)
-            }
-        }
-        catch (e: FileNotFoundException) {
-            null
-        }
+        readObjectFromJsonFile(path, StartupInfo::class.java)
     }
 }
