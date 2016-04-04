ac2da34d1bd2232395cbcd9b8e22d015f15fb11e
guillaume <guillaume.reeves@gmail.com>
2016-04-04 14:56:52 -0400

    fixed content height on add contact page

--- ui/ui/js/internal/addContact.js
+++ ui/ui/js/internal/addContact.js
@@ -2,4 +2,7 @@ $(function(){
     document.getElementById("username").focus();
 
     KEYTAP.contactController.newContactEvent();
+
+    var height = window.innerHeight - 52;
+    $("#content").css("min-height", height + "px");
 });
