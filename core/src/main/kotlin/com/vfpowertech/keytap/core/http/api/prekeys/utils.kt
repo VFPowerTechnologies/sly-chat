88ada2893c2c6e6d9e28d15cc558c7a2480995f0
Kevin Tardif <kevin@vfpowertech.com>
2016-04-05 16:11:17 -0400

    Fixed prekey upload data

--- core/src/main/kotlin/com/vfpowertech/keytap/core/http/api/prekeys/utils.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/http/api/prekeys/utils.kt
@@ -1,22 +1,61 @@
 @file:JvmName("PreKeyUtils")
 package com.vfpowertech.keytap.core.http.api.prekeys
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.vfpowertech.keytap.core.crypto.KeyVault
 import com.vfpowertech.keytap.core.crypto.hexify
 import com.vfpowertech.keytap.core.crypto.signal.GeneratedPreKeys
 import com.vfpowertech.keytap.core.crypto.signal.UserPreKeySet
 import com.vfpowertech.keytap.core.crypto.unhexify
+import org.whispersystems.libsignal.ecc.Curve
+import org.whispersystems.libsignal.ecc.ECPublicKey
 import org.whispersystems.libsignal.state.PreKeyRecord
 import org.whispersystems.libsignal.state.SignedPreKeyRecord
 
+data class UnsignedPreKeyPublicData(
+    @JsonProperty("id")
+    val id: Int,
+    @JsonProperty("key")
+    val key: ByteArray
+) {
+    fun getECPublicKey(): ECPublicKey =
+        Curve.decodePoint(key, 0)
+}
+
+fun PreKeyRecord.toPublicData(): UnsignedPreKeyPublicData =
+    UnsignedPreKeyPublicData(
+        id,
+        keyPair.publicKey.serialize()
+    )
+
+data class SignedPreKeyPublicData(
+    @JsonProperty("id")
+    val id: Int,
+    @JsonProperty("key")
+    val key: ByteArray,
+    @JsonProperty("signature")
+    val signature: ByteArray
+) {
+    fun getECPublicKey(): ECPublicKey =
+        Curve.decodePoint(key, 0)
+}
+
+fun SignedPreKeyRecord.toPublicData(): SignedPreKeyPublicData =
+    SignedPreKeyPublicData(
+        id,
+        keyPair.publicKey.serialize(),
+        signature
+    )
+
 fun serializePreKey(preKeyRecord: PreKeyRecord): String =
-    preKeyRecord.serialize().hexify()
+    ObjectMapper().writeValueAsString(preKeyRecord.toPublicData())
 
 fun serializeOneTimePreKeys(oneTimePreKeys: List<PreKeyRecord>): List<String> =
     oneTimePreKeys.map { serializePreKey(it) }
 
 fun serializeSignedPreKey(signedPreKeyRecord: SignedPreKeyRecord): String =
-    signedPreKeyRecord.serialize().hexify()
+    ObjectMapper().writeValueAsString(signedPreKeyRecord.toPublicData())
 
 fun preKeyStorageRequestFromGeneratedPreKeys(
     authToken: String,
