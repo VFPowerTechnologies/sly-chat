8f0c119ca863e23c08f6bed45cc1f5497d973151
guillaume <guillaume.reeves@gmail.com>
2016-04-11 12:05:43 -0400

    Removed useless promise return

--- android/src/main/kotlin/com/vfpowertech/keytap/android/AndroidWindowService.kt
+++ android/src/main/kotlin/com/vfpowertech/keytap/android/AndroidWindowService.kt
@@ -3,7 +3,6 @@ package com.vfpowertech.keytap.android
 import android.content.Context
 import android.view.inputmethod.InputMethodManager
 import com.vfpowertech.keytap.services.ui.UIWindowService
-import nl.komponents.kovenant.Promise
 
 class AndroidWindowService(private val context: Context) : UIWindowService {
     override fun minimize() {
@@ -13,7 +12,7 @@ class AndroidWindowService(private val context: Context) : UIWindowService {
         activity.moveTaskToBack(true)
     }
 
-    override fun closeSoftKeyboard(): Promise<Unit, Exception> {
+    override fun closeSoftKeyboard() {
         val androidApp = AndroidApp.get(context)
 
         val currentFocus = androidApp.currentActivity?.currentFocus
@@ -22,7 +21,5 @@ class AndroidWindowService(private val context: Context) : UIWindowService {
             val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
         }
-
-        return Promise.ofSuccess(Unit)
     }
 }
--- desktop/src/main/kotlin/com/vfpowertech/keytap/desktop/DesktopWindowService.kt
+++ desktop/src/main/kotlin/com/vfpowertech/keytap/desktop/DesktopWindowService.kt
@@ -2,13 +2,12 @@ package com.vfpowertech.keytap.desktop
 
 import com.vfpowertech.keytap.services.ui.UIWindowService
 import javafx.stage.Stage
-import nl.komponents.kovenant.Promise
 
 class DesktopWindowService(private val stage: Stage) : UIWindowService {
     override fun minimize() {
         stage.isIconified = true
     }
 
-    override fun closeSoftKeyboard(): Promise<Unit, Exception> = Promise.ofSuccess(Unit)
+    override fun closeSoftKeyboard() = Unit
 
 }
