50de4a0810428627307f0309f6999d260990ad14
Kevin Tardif <kevin@vfpowertech.com>
2016-04-19 13:57:26 -0400

    Fixed init-related issues

--- ui-services/src/main/kotlin/com/vfpowertech/keytap/services/AuthResult.kt
+++ ui-services/src/main/kotlin/com/vfpowertech/keytap/services/AuthResult.kt
@@ -3,4 +3,4 @@ package com.vfpowertech.keytap.services
 import com.vfpowertech.keytap.core.crypto.KeyVault
 import com.vfpowertech.keytap.core.persistence.AccountInfo
 
-data class AuthResult(val authToken: String?, val keyRegenCount: Int, val keyVault: KeyVault, val accountInfo: AccountInfo)
+data class AuthResult(val authToken: String, val keyRegenCount: Int, val keyVault: KeyVault, val accountInfo: AccountInfo)
