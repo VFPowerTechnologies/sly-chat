fdd570cb3c4bfa67f67c8c03add88a504e805edb
Kevin Tardif <kevin@vfpowertech.com>
2016-04-21 11:56:27 -0400

    Added AccountInfoPersistenceManager.retrieveSync

--- core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/AccountInfoPersistenceManager.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/AccountInfoPersistenceManager.kt
@@ -4,6 +4,7 @@ import nl.komponents.kovenant.Promise
 
 /** Manages persisting account info. Must not be encrypted. */
 interface AccountInfoPersistenceManager {
+    fun retrieveSync(): AccountInfo?
     fun retrieve(): Promise<AccountInfo?, Exception>
     fun store(accountInfo: AccountInfo): Promise<Unit, Exception>
 }
--- core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/json/JsonAccountInfoPersistenceManager.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/persistence/json/JsonAccountInfoPersistenceManager.kt
@@ -7,8 +7,12 @@ import nl.komponents.kovenant.task
 import java.io.File
 
 class JsonAccountInfoPersistenceManager(private val path: File) : AccountInfoPersistenceManager {
+    override fun retrieveSync(): AccountInfo? {
+        return readObjectFromJsonFile(path, AccountInfo::class.java)
+    }
+
     override fun retrieve(): Promise<AccountInfo?, Exception> = task {
-        readObjectFromJsonFile(path, AccountInfo::class.java)
+        retrieveSync()
     }
 
     override fun store(accountInfo: AccountInfo): Promise<Unit, Exception> = task {
