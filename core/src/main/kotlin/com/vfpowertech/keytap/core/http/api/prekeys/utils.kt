edb91d56de1f0d7c31327c6d47fa5c478b9d203e
Kevin Tardif <kevin@vfpowertech.com>
2016-04-28 10:20:28 -0400

    Stray println

--- core/src/main/kotlin/com/vfpowertech/keytap/core/http/api/prekeys/utils.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/http/api/prekeys/utils.kt
@@ -79,7 +79,7 @@ fun preKeyStorageRequestFromGeneratedPreKeys(
 fun SerializedPreKeySet.toPreKeyBundle(deviceId: Int): PreKeyBundle {
     val objectMapper = ObjectMapper()
     val registrationId = registrationId
-    println(preKey)
+    
     val oneTimePreKey = objectMapper.readValue(preKey, UnsignedPreKeyPublicData::class.java)
     val signedPreKey = objectMapper.readValue(signedPreKey, SignedPreKeyPublicData::class.java)
     return PreKeyBundle(
