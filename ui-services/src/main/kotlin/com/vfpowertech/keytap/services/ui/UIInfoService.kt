f0d21eb7bafc43c7a1ea6c182970f0129c87057a
guillaume <guillaume.reeves@gmail.com>
2016-05-03 16:45:21 -0400

    fix proper indentation

--- ui-services/src/main/kotlin/com/vfpowertech/keytap/services/ui/UIInfoService.kt
+++ ui-services/src/main/kotlin/com/vfpowertech/keytap/services/ui/UIInfoService.kt
@@ -1,10 +1,8 @@
 package com.vfpowertech.keytap.services.ui
 
 import com.vfpowertech.jsbridge.processor.annotations.JSToJavaGenerate
-import com.vfpowertech.keytap.services.LoginEvent
 import nl.komponents.kovenant.Promise
 
-/** Responsible for authentication */
 @JSToJavaGenerate("InfoService")
 interface UIInfoService {
     /** Get the user country based on his ip. */
