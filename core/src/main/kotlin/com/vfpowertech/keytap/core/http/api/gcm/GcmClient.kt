2e92a49f4614c2263fd573b325f62152ca043b02
Kevin Tardif <kevin@vfpowertech.com>
2016-05-10 11:48:17 -0400

    Updated GcmClient

--- core/src/main/kotlin/com/vfpowertech/keytap/core/http/api/gcm/GcmClient.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/http/api/gcm/GcmClient.kt
@@ -3,17 +3,18 @@ package com.vfpowertech.keytap.core.http.api.gcm
 import com.vfpowertech.keytap.core.http.HttpClient
 import com.vfpowertech.keytap.core.http.api.ApiResult
 import com.vfpowertech.keytap.core.http.api.EmptyResponse
-import com.vfpowertech.keytap.core.http.api.apiPostRequest
+import com.vfpowertech.keytap.core.http.api.apiPostRequest2
+import com.vfpowertech.keytap.core.relay.UserCredentials
 import com.vfpowertech.keytap.core.typeRef
 
 class GcmClient(private val serverBaseUrl: String, private val httpClient: HttpClient) {
-    fun register(request: RegisterRequest): RegisterResponse {
+    fun register(userCredentials: UserCredentials, request: RegisterRequest): RegisterResponse {
         val url = "$serverBaseUrl/v1/gcm/register"
-        return apiPostRequest(httpClient, url, request, setOf(200, 400, 403), typeRef())
+        return apiPostRequest2(httpClient, url, userCredentials, request, setOf(200, 400, 403), typeRef())
     }
 
-    fun unregister(request: UnregisterRequest) {
+    fun unregister(userCredentials: UserCredentials, request: UnregisterRequest) {
         val url = "$serverBaseUrl/v1/gcm/unregister"
-        apiPostRequest(httpClient, url, request, setOf(200), typeRef<ApiResult<EmptyResponse>>())
+        apiPostRequest2(httpClient, url, userCredentials, request, setOf(200), typeRef<ApiResult<EmptyResponse>>())
     }
 }
--- core/src/main/kotlin/com/vfpowertech/keytap/core/http/api/gcm/RegisterRequest.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/http/api/gcm/RegisterRequest.kt
@@ -1,10 +1,6 @@
 package com.vfpowertech.keytap.core.http.api.gcm
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class RegisterRequest(
-    @get:JsonProperty("auth-token")
-    val authToken: String,
     val token: String,
     val installationId: String
 )
