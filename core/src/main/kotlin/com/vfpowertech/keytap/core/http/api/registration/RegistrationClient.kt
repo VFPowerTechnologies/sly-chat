f46457b16f61b7fedf94cedd7c2e846b876587ce
Kevin Tardif <kevin@vfpowertech.com>
2016-05-10 11:41:26 -0400

    Updated RegistrationClient

--- core/src/main/kotlin/com/vfpowertech/keytap/core/http/api/registration/RegistrationClient.kt
+++ core/src/main/kotlin/com/vfpowertech/keytap/core/http/api/registration/RegistrationClient.kt
@@ -3,7 +3,7 @@ package com.vfpowertech.keytap.core.http.api.registration
 import com.vfpowertech.keytap.core.http.HttpClient
 import com.vfpowertech.keytap.core.http.api.accountupdate.UpdatePhoneRequest
 import com.vfpowertech.keytap.core.http.api.accountupdate.UpdatePhoneResponse
-import com.vfpowertech.keytap.core.http.api.apiPostRequest
+import com.vfpowertech.keytap.core.http.api.apiPostRequest2
 import com.vfpowertech.keytap.core.typeRef
 
 /**
@@ -13,24 +13,24 @@ class RegistrationClient(private val serverBaseUrl: String, private val httpClie
     fun register(request: RegisterRequest): RegisterResponse {
         val url = "$serverBaseUrl/v1/register"
 
-        return apiPostRequest(httpClient, url, request, setOf(200, 400), typeRef())
+        return apiPostRequest2(httpClient, url, null, request, setOf(200, 400), typeRef())
     }
 
     fun verifySmsCode(request: SmsVerificationRequest): SmsVerificationResponse {
         val url = "$serverBaseUrl/v1/sms/verification"
 
-        return apiPostRequest(httpClient, url, request, setOf(200, 400), typeRef())
+        return apiPostRequest2(httpClient, url, null, request, setOf(200, 400), typeRef())
     }
 
     fun resendSmsCode(request: SmsResendRequest): SmsVerificationResponse {
         val url = "$serverBaseUrl/v1/sms/resend"
 
-        return apiPostRequest(httpClient, url, request, setOf(200, 400), typeRef())
+        return apiPostRequest2(httpClient, url, null, request, setOf(200, 400), typeRef())
     }
 
     fun updatePhone(request: UpdatePhoneRequest): UpdatePhoneResponse {
         val url = "$serverBaseUrl/v1/account/update/phone"
 
-        return apiPostRequest(httpClient, url, request, setOf(200, 400), typeRef())
+        return apiPostRequest2(httpClient, url, null, request, setOf(200, 400), typeRef())
     }
 }
