2fb86908ec2bbaeac8c032e27e6e03c5d7fd0760
Kevin Tardif <kevin@vfpowertech.com>
2016-05-17 15:30:42 -0400

    Added clipboard methods + android impl

--- desktop/src/main/kotlin/io/slychat/messenger/desktop/DesktopWindowService.kt
+++ desktop/src/main/kotlin/io/slychat/messenger/desktop/DesktopWindowService.kt
@@ -4,6 +4,12 @@ import io.slychat.messenger.services.ui.UIWindowService
 import javafx.stage.Stage
 
 class DesktopWindowService(private val stage: Stage) : UIWindowService {
+    override fun copyTextToClipboard(text: String) {}
+
+    override fun getTextFromClipboard(): String? {
+        return null
+    }
+
     override fun minimize() {
         stage.isIconified = true
     }
