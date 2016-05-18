f8b28c18f2c140db9e3101f41a9c6c7be269c43a
Kevin Tardif <kevin@vfpowertech.com>
2016-05-18 11:05:00 -0400

    Properly shutting down netty event loops

--- core/src/main/kotlin/io/slychat/messenger/core/relay/base/netty/NettyRelayConnector.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/relay/base/netty/NettyRelayConnector.kt
@@ -1,13 +1,13 @@
 package io.slychat.messenger.core.relay.base.netty
 
-import io.slychat.messenger.core.relay.base.RelayConnectionEstablished
-import io.slychat.messenger.core.relay.base.RelayConnectionEvent
-import io.slychat.messenger.core.relay.base.RelayConnector
 import io.netty.bootstrap.Bootstrap
 import io.netty.channel.ChannelFutureListener
 import io.netty.channel.ChannelOption
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioSocketChannel
+import io.slychat.messenger.core.relay.base.RelayConnectionEstablished
+import io.slychat.messenger.core.relay.base.RelayConnectionEvent
+import io.slychat.messenger.core.relay.base.RelayConnector
 import nl.komponents.kovenant.deferred
 import rx.Observable
 import java.net.InetSocketAddress
@@ -17,19 +17,28 @@ class NettyRelayConnector : RelayConnector {
         Observable.create({ subscriber ->
             val sslHandshakeComplete = deferred<Boolean, Exception>()
 
+            val eventLoopGroup = NioEventLoopGroup()
+
             val bootstrap = Bootstrap()
-                .group(NioEventLoopGroup())
+                .group(eventLoopGroup)
                 .channel(NioSocketChannel::class.java)
                 .option(ChannelOption.SO_KEEPALIVE, true)
                 .handler(RelayConnectionInitializer(subscriber, sslHandshakeComplete))
 
             val channelFuture = bootstrap.connect(address)
             channelFuture.addListener(ChannelFutureListener { cf ->
-                if (!cf.isSuccess)
+                if (!cf.isSuccess) {
                     subscriber.onError(cf.cause())
+                    eventLoopGroup.shutdownGracefully()
+                }
                 else {
                     //if this fails, exceptionCaught is called, which'll trigger subscriber.onError
                     val channel = cf.channel()
+
+                    channel.closeFuture().addListener {
+                        eventLoopGroup.shutdownGracefully()
+                    }
+
                     sslHandshakeComplete.promise success { successful ->
                         if (successful)
                             subscriber.onNext(RelayConnectionEstablished(NettyRelayConnection(channel)))
