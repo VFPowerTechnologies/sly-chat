ce381d0917353ef3b037cc06d726431f592a7766
Kevin Tardif <kevin@vfpowertech.com>
2016-05-20 15:38:44 -0400

    Supressing unused warnings for classes used via reflection

--- core/src/main/kotlin/io/slychat/messenger/core/persistence/sqlite/migrations/DatabaseMigration0.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/persistence/sqlite/migrations/DatabaseMigration0.kt
@@ -2,4 +2,5 @@ package io.slychat.messenger.core.persistence.sqlite.migrations
 
 import io.slychat.messenger.core.persistence.sqlite.DatabaseMigration
 
+@Suppress("unused")
 class DatabaseMigration0 : DatabaseMigration(0)
--- core/src/main/kotlin/io/slychat/messenger/core/persistence/sqlite/migrations/DatabaseMigration1.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/persistence/sqlite/migrations/DatabaseMigration1.kt
@@ -2,4 +2,5 @@ package io.slychat.messenger.core.persistence.sqlite.migrations
 
 import io.slychat.messenger.core.persistence.sqlite.DatabaseMigration
 
+@Suppress("unused")
 class DatabaseMigration1 : DatabaseMigration(1, listOf("package_queue"))
