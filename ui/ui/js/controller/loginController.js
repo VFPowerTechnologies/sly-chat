5f15b285623bd76bb73fbc9f4839c0b5d5702d88
Guillaume <guillaume.reeves@gmail.com>
2016-05-27 10:13:40 -0400

    fix element not found on login failure.

--- ui/ui/js/controller/loginController.js
+++ ui/ui/js/controller/loginController.js
@@ -4,7 +4,13 @@ var LoginController = function(model){
 };
 
 LoginController.prototype = {
+    /**
+     * Init function, ran only once when login controller is initiated.
+     */
     init : function() {
+        /**
+         * Click event for submit login button.
+         */
         $(document).on("click", "#submitLoginBtn", function(e){
             e.preventDefault();
 
@@ -23,6 +29,9 @@ LoginController.prototype = {
             }
         }.bind(this));
 
+        /**
+         * Click event for go to registration button.
+         */
         $(document).on("click", "#registrationGoBtn", function (e) {
             e.preventDefault();
             KEYTAP.navigationController.loadPage("register.html");
@@ -30,6 +39,11 @@ LoginController.prototype = {
 
         loginService.addLoginEventListener(this.onLoginEvent.bind(this));
     },
+    /**
+     * Login event handler.
+     *
+     * @param ev
+     */
     onLoginEvent : function(ev) {
         switch(ev.state) {
             case "LOGGED_OUT":
@@ -48,6 +62,11 @@ LoginController.prototype = {
                 break;
         }
     },
+    /**
+     * Handle login successful event.
+     *
+     * @param ev
+     */
     onLoginSuccessful : function(ev) {
         var accountInfo = ev.accountInfo;
         var publicKey = ev.publicKey;
@@ -66,6 +85,11 @@ LoginController.prototype = {
             }
         }.bind(this));
     },
+    /**
+     * Handle login failure event.
+     *
+     * @param ev
+     */
     onLoginFailure : function(ev) {
         var errorMessage = ev.errorMessage;
         if(errorMessage !== null) {
@@ -82,13 +106,18 @@ LoginController.prototype = {
             }
         }
         else {
+            KEYTAP.navigationController.loadPage("login.html");
             this.setInfo("", "");
             this.modal.close();
-            document.getElementById("login-error").innerHTML = "<li>An unexpected error occurred</li>";
-            console.log("An unexpected error occured: " + ev);
+            $("#login-error").html("<li>An unexpected error occurred</li>");
+            console.log("An unexpected error occured: ");
+            console.log(ev);
             $("#submitLoginBtn").prop("disabled", false);
         }
     },
+    /**
+     * Handle on logout event.
+     */
     onLogout : function() {
         KEYTAP.navigationController.clearHistory();
         KEYTAP.contactController.model.resetContacts();
@@ -104,30 +133,54 @@ LoginController.prototype = {
         //do this last, so that everything's been reset before loading a new page
         KEYTAP.navigationController.loadPage("login.html");
     },
+    /**
+     * Handle submiting login to the backend.
+     *
+     * @param rememberMe
+     */
     login : function(rememberMe) {
         if(typeof rememberMe === "undefined")
             rememberMe = $("#rememberMe").is(':checked');
 
         loginService.login(this.model.getLogin(), this.model.getPassword(), rememberMe);
     },
+    /**
+     * Set login and password in cache to use during login process.
+     * @param login
+     * @param password
+     */
     setInfo : function(login, password) {
         this.model.setItems({
             "login" : login,
             "password" : password
         })
     },
+    /**
+     * Handle login out with the backend.
+     */
     logout : function () {
         loginService.logout().then(function () {
             this.clearUiCacheOnLogout();
         }.bind(this));
     },
+    /**
+     * Handle clearing the model cache.
+     */
     clearCache : function () {
         this.model.clearCache();
     },
+    /**
+     * Create the login status dialog.
+     *
+     * @returns {*}
+     */
     createLoginModal: function () {
         var html = "<div style='text-align: center;'> <h6 style='margin-bottom: 15px; color: whitesmoke;'>We are logging you in</h6> <i class='fa fa-spinner fa-3x fa-spin'></i> </div>";
         return createStatusModal(html);
     },
+    /**
+     * Handle clearing the whole UI cache on logout.
+     */
     clearUiCacheOnLogout : function () {
         KEYTAP.chatController.clearCache();
         KEYTAP.contactController.clearCache();
