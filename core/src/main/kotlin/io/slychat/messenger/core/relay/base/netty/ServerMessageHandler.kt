5b4f9f84a9632f50825f98e07ff3cb47e842d8d1
Kevin Tardif <kevin@vfpowertech.com>
2016-06-14 14:20:16 -0400

    TLS hostname verification for relay connector

--- core/src/main/kotlin/io/slychat/messenger/core/relay/base/netty/ServerMessageHandler.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/relay/base/netty/ServerMessageHandler.kt
@@ -1,15 +1,17 @@
 package io.slychat.messenger.core.relay.base.netty
 
-import io.slychat.messenger.core.relay.base.*
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.ByteToMessageDecoder
 import io.netty.handler.ssl.SslHandler
-import io.netty.util.concurrent.GenericFutureListener
-import io.slychat.messenger.core.relay.base.Header
-import io.slychat.messenger.core.relay.base.RelayConnectionEvent
+import io.netty.handler.ssl.SslHandshakeCompletionEvent
+import io.slychat.messenger.core.BuildConfig
+import io.slychat.messenger.core.relay.base.*
 import nl.komponents.kovenant.Deferred
 import rx.Observer
+import java.net.InetSocketAddress
+import java.security.cert.CertificateException
+import javax.net.ssl.HttpsURLConnection
 
 /** Handles converting received server messages into message instances. */
 class ServerMessageHandler(
@@ -19,13 +21,30 @@ class ServerMessageHandler(
     private var lastHeader: Header? = null
     private var observerableComplete = false
 
-    override fun channelActive(ctx: ChannelHandlerContext) {
-        super.channelActive(ctx)
+    override fun userEventTriggered(ctx: ChannelHandlerContext, evt: Any) {
+        if (evt is SslHandshakeCompletionEvent) {
+            if (!evt.isSuccess) {
+                //in this case, error'll call exceptionCaught, so no need to care
+                sslHandshakeComplete.resolve(false)
+                return
+            }
 
-        ctx.pipeline().get(SslHandler::class.java).handshakeFuture().addListener(GenericFutureListener { future ->
-            //error'll call exceptionCaught, so no need to care
-            sslHandshakeComplete.resolve(future.isSuccess)
-        })
+            val hostname = (ctx.channel().remoteAddress() as InetSocketAddress).hostName
+
+            val isHostVerified = if (BuildConfig.DISABLE_HOST_VERIFICATION)
+                true
+            else {
+                val sslHandler = ctx.pipeline().get(SslHandler::class.java)
+                val session = sslHandler.engine().session
+                HttpsURLConnection.getDefaultHostnameVerifier().verify(hostname, session)
+            }
+
+            sslHandshakeComplete.resolve(isHostVerified)
+
+            //mimic HttpsURLConnection's behavior
+            if (!isHostVerified)
+                throw CertificateException("No name matching $hostname found")
+        }
     }
 
     override fun decode(ctx: ChannelHandlerContext, `in`: ByteBuf, out: MutableList<Any>) {
