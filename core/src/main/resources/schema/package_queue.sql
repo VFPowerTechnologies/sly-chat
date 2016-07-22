ed02f70513a4bef59a842f9e8a4d0958be3b465c
Kevin Tardif <kevin@vfpowertech.com>
2016-07-22 13:17:42 -0400

    Simplified unique index decl in package_queue.sql

--- core/src/main/resources/schema/package_queue.sql
+++ core/src/main/resources/schema/package_queue.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS package_queue (
     device_id INTEGER NOT NULL,
     message_id TEXT NOT NULL,
     timestamp INTEGER NOT NULL,
-    payload TEXT NOT NULL
-);
+    payload TEXT NOT NULL,
 
-CREATE UNIQUE INDEX IF NOT EXISTS `unique_package_queue_user_id_message_id` ON `package_queue` (user_id, message_id);
+    UNIQUE (user_id, message_id)
+);
