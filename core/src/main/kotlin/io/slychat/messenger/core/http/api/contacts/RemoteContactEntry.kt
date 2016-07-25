d617e4340a07d39e31023768bfe47dd8494d8f5a
Kevin Tardif <kevin@vfpowertech.com>
2016-07-25 15:33:06 -0400

    Updated remote contact format

--- core/src/main/kotlin/io/slychat/messenger/core/http/api/contacts/RemoteContactEntry.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/http/api/contacts/RemoteContactEntry.kt
@@ -1,10 +1,21 @@
 package io.slychat.messenger.core.http.api.contacts
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.slychat.messenger.core.UserId
+import io.slychat.messenger.core.persistence.AllowedMessageLevel
 
-data class RemoteContactEntry(
+/** Represents a saved remote contact entry. */
+data class RemoteContactEntryData(
+    @JsonProperty("userId")
+    val userId: UserId,
+    @JsonProperty("allowedMessageLevel")
+    val allowedMessageLevel: AllowedMessageLevel
+)
+
+/** Encrypted [RemoteContactEntryData] and user ID hash uploaded to remote server. */
+class RemoteContactEntry(
     @JsonProperty("hash")
     val hash: String,
-    @JsonProperty("encryptedUserId")
-    val encryptedUserId: String
+    @JsonProperty("encryptedContactData")
+    val encryptedContactData: ByteArray
 )
--- core/src/main/kotlin/io/slychat/messenger/core/http/api/contacts/UpdateContactsRequest.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/http/api/contacts/UpdateContactsRequest.kt
@@ -1,6 +1,5 @@
 package io.slychat.messenger.core.http.api.contacts
 
 data class UpdateContactsRequest(
-    val add: List<RemoteContactEntry>,
-    val remove: List<String>
+    val contacts: List<RemoteContactEntry>
 )
