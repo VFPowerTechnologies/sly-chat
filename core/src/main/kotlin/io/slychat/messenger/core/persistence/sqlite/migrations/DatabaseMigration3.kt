a59205bf761b4edb1c8f1cefdd956586d49c334e
Kevin Tardif <kevin@vfpowertech.com>
2016-08-01 13:55:31 -0400

    Fixed old migration ctor usage

--- core/src/main/kotlin/io/slychat/messenger/core/persistence/sqlite/migrations/DatabaseMigration3.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/persistence/sqlite/migrations/DatabaseMigration3.kt
@@ -3,4 +3,4 @@ package io.slychat.messenger.core.persistence.sqlite.migrations
 import io.slychat.messenger.core.persistence.sqlite.DatabaseMigration
 
 @Suppress("unused")
-class DatabaseMigration3 : DatabaseMigration(3, listOf("remote_contact_updates"))
+class DatabaseMigration3 : DatabaseMigration(3)
