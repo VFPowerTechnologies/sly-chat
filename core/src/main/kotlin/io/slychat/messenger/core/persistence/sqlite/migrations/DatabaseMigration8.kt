feb8767e681d7c5ccbbc9899ee1e5fa55f377db6
Kevin Tardif <kevin@vfpowertech.com>
2016-08-01 13:32:16 -0400

    Bumped db version to 9

---
+++ core/src/main/kotlin/io/slychat/messenger/core/persistence/sqlite/migrations/DatabaseMigration8.kt
@@ -0,0 +1,6 @@
+package io.slychat.messenger.core.persistence.sqlite.migrations
+
+import io.slychat.messenger.core.persistence.sqlite.DatabaseMigration
+
+@Suppress("unused")
+class DatabaseMigration8 : DatabaseMigration(8)
