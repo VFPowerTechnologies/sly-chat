6af9dfe3022c778ff315d29d13ac1e6814ad0d11
Kevin Tardif <kevin@vfpowertech.com>
2016-08-03 15:53:16 -0400

    Added temporary workaround for restricted cryptography on windows/osx

--- desktop/src/main/kotlin/io/slychat/messenger/desktop/DesktopApp.kt
+++ desktop/src/main/kotlin/io/slychat/messenger/desktop/DesktopApp.kt
@@ -27,6 +27,9 @@ import nl.komponents.kovenant.jfx.JFXDispatcher
 import nl.komponents.kovenant.ui.KovenantUi
 import org.slf4j.LoggerFactory
 import rx.schedulers.JavaFxScheduler
+import java.security.Permission
+import java.security.PermissionCollection
+import javax.crypto.Cipher
 
 class DesktopApp : Application() {
     private val app: SlyApplication = SlyApplication()
@@ -153,7 +156,51 @@ class DesktopApp : Application() {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            removeCryptographyRestrictions()
             launch(DesktopApp::class.java, *args)
+        }
+
+        private fun isRestrictedCryptography(): Boolean {
+            return Cipher.getMaxAllowedKeyLength("AES") != Integer.MAX_VALUE
+        }
+
+        //taken from: https://stackoverflow.com/questions/1179672
+        //this is currently just a development workaround; not sure how legal this is, and starting from 1.8u102
+        //JceSecurity.isRestricted is now final so this is no longer usable
+        private fun removeCryptographyRestrictions() {
+            if (!isRestrictedCryptography())
+                return
+
+            val log = LoggerFactory.getLogger(DesktopApp::class.java)
+            log.info("Detected restricted cryptography, attempting to remove restriction")
+
+            try {
+                val jceSecurity = Class.forName("javax.crypto.JceSecurity")
+                val cryptoPermissions = Class.forName("javax.crypto.CryptoPermissions")
+                val cryptoAllPermission = Class.forName("javax.crypto.CryptoAllPermission")
+
+                val isRestrictedField = jceSecurity.getDeclaredField("isRestricted")
+                isRestrictedField.isAccessible = true
+                isRestrictedField.set(null, false)
+
+                val defaultPolicyField = jceSecurity.getDeclaredField("defaultPolicy")
+                defaultPolicyField.isAccessible = true
+                val defaultPolicy = defaultPolicyField.get(null) as PermissionCollection
+
+                val perms = cryptoPermissions.getDeclaredField("perms")
+                perms.isAccessible = true
+                (perms.get(defaultPolicy) as MutableMap<*, *>).clear()
+
+                val instance = cryptoAllPermission.getDeclaredField("INSTANCE")
+                instance.isAccessible = true
+                defaultPolicy.add(instance.get(null) as Permission)
+            }
+            catch (e: Exception) {
+                //libsignal requires 256bit AES key support
+                throw RuntimeException("Unable to remove cryptographic restrictions: ${e.message}", e)
+            }
+
+            log.info("Restrictions successfully removed")
         }
     }
 
