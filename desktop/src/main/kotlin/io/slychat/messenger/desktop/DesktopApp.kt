7f4acfa80d8752372d57af23a38a59beadbcff65
Kevin Tardif <kevin@vfpowertech.com>
2016-08-05 11:17:02 -0400

    Removed removeCryptographyRestrictions hack

--- desktop/src/main/kotlin/io/slychat/messenger/desktop/DesktopApp.kt
+++ desktop/src/main/kotlin/io/slychat/messenger/desktop/DesktopApp.kt
@@ -15,7 +15,9 @@ import io.slychat.messenger.services.ui.createAppDirectories
 import io.slychat.messenger.services.ui.registerCoreServicesOnDispatcher
 import javafx.animation.FadeTransition
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.scene.Scene
+import javafx.scene.control.Alert
 import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
 import javafx.scene.shape.Rectangle
@@ -27,8 +29,6 @@ import nl.komponents.kovenant.jfx.JFXDispatcher
 import nl.komponents.kovenant.ui.KovenantUi
 import org.slf4j.LoggerFactory
 import rx.schedulers.JavaFxScheduler
-import java.security.Permission
-import java.security.PermissionCollection
 import javax.crypto.Cipher
 
 class DesktopApp : Application() {
@@ -72,6 +72,17 @@ class DesktopApp : Application() {
     }
 
     override fun start(primaryStage: Stage) {
+        //libsignal requires AES256 for message encryption+decryption
+        if (isRestrictedCryptography()) {
+            val alert = Alert(Alert.AlertType.ERROR)
+            alert.title = "Unable to start Sly Chat"
+            alert.headerText = "An error occurred during initialization."
+            alert.contentText = "Restricted JCE policy detected. Please install Unlimited Strength Jurisdiction Policy Files from Oracle."
+            alert.showAndWait()
+            Platform.exit()
+            return
+        }
+
         stage = primaryStage
 
         KovenantUi.uiContext {
@@ -156,51 +167,11 @@ class DesktopApp : Application() {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            removeCryptographyRestrictions()
             launch(DesktopApp::class.java, *args)
         }
 
         private fun isRestrictedCryptography(): Boolean {
             return Cipher.getMaxAllowedKeyLength("AES") != Integer.MAX_VALUE
-        }
-
-        //taken from: https://stackoverflow.com/questions/1179672
-        //this is currently just a development workaround; not sure how legal this is, and starting from 1.8u102
-        //JceSecurity.isRestricted is now final so this is no longer usable
-        private fun removeCryptographyRestrictions() {
-            if (!isRestrictedCryptography())
-                return
-
-            val log = LoggerFactory.getLogger(DesktopApp::class.java)
-            log.info("Detected restricted cryptography, attempting to remove restriction")
-
-            try {
-                val jceSecurity = Class.forName("javax.crypto.JceSecurity")
-                val cryptoPermissions = Class.forName("javax.crypto.CryptoPermissions")
-                val cryptoAllPermission = Class.forName("javax.crypto.CryptoAllPermission")
-
-                val isRestrictedField = jceSecurity.getDeclaredField("isRestricted")
-                isRestrictedField.isAccessible = true
-                isRestrictedField.set(null, false)
-
-                val defaultPolicyField = jceSecurity.getDeclaredField("defaultPolicy")
-                defaultPolicyField.isAccessible = true
-                val defaultPolicy = defaultPolicyField.get(null) as PermissionCollection
-
-                val perms = cryptoPermissions.getDeclaredField("perms")
-                perms.isAccessible = true
-                (perms.get(defaultPolicy) as MutableMap<*, *>).clear()
-
-                val instance = cryptoAllPermission.getDeclaredField("INSTANCE")
-                instance.isAccessible = true
-                defaultPolicy.add(instance.get(null) as Permission)
-            }
-            catch (e: Exception) {
-                //libsignal requires 256bit AES key support
-                throw RuntimeException("Unable to remove cryptographic restrictions: ${e.message}", e)
-            }
-
-            log.info("Restrictions successfully removed")
         }
     }
 
