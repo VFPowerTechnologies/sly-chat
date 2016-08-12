eae1a326cf254950e6a4b06e1e8c932c999fda30
Kevin Tardif <kevin@vfpowertech.com>
2016-08-12 16:12:09 -0400

    Some logging to AccountInfoManagerImpl

--- ui-services/src/main/kotlin/io/slychat/messenger/services/AccountInfoManagerImpl.kt
+++ ui-services/src/main/kotlin/io/slychat/messenger/services/AccountInfoManagerImpl.kt
@@ -20,7 +20,10 @@ class AccountInfoManagerImpl(
         get() = updatesSubject
 
     override fun update(newAccountInfo: AccountInfo): Promise<Unit, Exception> {
+        log.debug("Updating account info to: {}", newAccountInfo)
+
         return accountInfoPersistenceManager.store(newAccountInfo) successUi {
+            log.debug("Account info updated")
             updatesSubject.onNext(newAccountInfo)
         } fail {
             log.error("Unable to update account info: {}", it.message, it)
