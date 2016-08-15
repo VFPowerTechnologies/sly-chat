e200720a302e02b6af4eb06d453aed4c89b4b2df
Kevin Tardif <kevin@vfpowertech.com>
2016-08-15 11:05:45 -0400

    AuthenticationService test for invalid local keyvault password

--- ui-services/src/test/kotlin/io/slychat/messenger/services/AuthenticationServiceTest.kt
+++ ui-services/src/test/kotlin/io/slychat/messenger/services/AuthenticationServiceTest.kt
@@ -2,8 +2,10 @@ package io.slychat.messenger.services
 
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
 import io.slychat.messenger.core.*
+import io.slychat.messenger.core.crypto.KeyVaultDecryptionFailedException
 import io.slychat.messenger.core.crypto.defaultRemotePasswordHashParams
 import io.slychat.messenger.core.crypto.generateNewKeyVault
 import io.slychat.messenger.core.http.api.authentication.*
@@ -95,19 +97,6 @@ class AuthenticationServiceTest {
     }
 
     @Test
-    fun `it should switch to remote auth if no key vault is available`() {
-        whenever(localAccountDirectory.findAccountFor(email)).thenReturn(accountInfo)
-        whenever(keyVaultPersistenceManager.retrieveSync(password)).thenReturn(null)
-
-        withSuccessfulRemoteAuth { authToken ->
-            val result = authenticationService.auth(email, password, registrationId).get()
-
-            assertEquals(accountInfo, result.accountInfo, "Invalid account info")
-            assertEquals(authToken, result.authToken, "Auth token is invalid")
-        }
-    }
-
-    @Test
     fun `it should switch to remote auth if no local data is available`() {
         whenever(localAccountDirectory.findAccountFor(any<String>())).thenReturn(null)
 
@@ -115,6 +104,30 @@ class AuthenticationServiceTest {
             val result = authenticationService.auth(email, password, registrationId).get()
             assertEquals(accountInfo, result.accountInfo, "Invalid account info")
             assertEquals(authToken, result.authToken, "Auth token is invalid")
+        }
+    }
+
+    @Test
+    fun `it should switch to remote auth if no key vault is available`() {
+        whenever(localAccountDirectory.findAccountFor(email)).thenReturn(accountInfo)
+        whenever(keyVaultPersistenceManager.retrieveSync(password)).thenReturn(null)
+
+        withSuccessfulRemoteAuth { authToken ->
+            authenticationService.auth(email, password, registrationId).get()
+
+            verify(authenticationClient).auth(any())
+        }
+    }
+
+    @Test
+    fun `it should attempt remote auth when local auth fails`() {
+        whenever(localAccountDirectory.findAccountFor(email)).thenReturn(accountInfo)
+        whenever(keyVaultPersistenceManager.retrieveSync(password)).thenThrow(KeyVaultDecryptionFailedException())
+
+        withSuccessfulRemoteAuth { authToken ->
+            authenticationService.auth(email, password, registrationId).get()
+
+            verify(authenticationClient).auth(any())
         }
     }
 }
