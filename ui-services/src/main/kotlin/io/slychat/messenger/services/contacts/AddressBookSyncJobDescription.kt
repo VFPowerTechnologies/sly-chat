c11fbd19178412292804a0f056147816c25d400f
Kevin Tardif <kevin@vfpowertech.com>
2016-08-25 09:33:15 -0400

    AddressBookSyncJobDescription documentation regarding diff steps

--- ui-services/src/main/kotlin/io/slychat/messenger/services/contacts/AddressBookSyncJobDescription.kt
+++ ui-services/src/main/kotlin/io/slychat/messenger/services/contacts/AddressBookSyncJobDescription.kt
@@ -1,6 +1,12 @@
 package io.slychat.messenger.services.contacts
 
-/** A description encapsulating an address book sync. */
+/**
+ * A description encapsulating an address book sync operation.
+ *
+ * Find platform contacts: Send platform contact entry data to the server in an attempt to find new contacts.
+ * Push: Push any pending AddressBookUpdates to the server.
+ * Pull: Pull down address book from server and add missing entries.
+ */
 class AddressBookSyncJobDescription {
     var push: Boolean = false
         private set
