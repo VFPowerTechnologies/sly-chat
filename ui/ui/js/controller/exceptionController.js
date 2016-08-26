cb3bd84474b615cf21afdf7c56a4386d198a6b2d
Guillaume Reeves <guillaume.reeves@gmail.com>
2016-08-26 14:59:25 -0400

    Fix exception dialog to be readable.

--- ui/ui/js/controller/exceptionController.js
+++ ui/ui/js/controller/exceptionController.js
@@ -5,26 +5,36 @@ ExceptionController.prototype = {
         console.log("An error occured");
         console.log(error);
 
-        // if (window.buildConfig.DEBUG == true) {
-        //     this.displayErrorModal(error);
-        // }
-        // else {
-        //     console.log(error);
-        // }
+        if (window.buildConfig.DEBUG == true) {
+            this.displayErrorModal(error);
+        }
+        else {
+            console.log(error);
+        }
 
     },
 
     displayErrorModal : function (error) {
-        slychat.modal({
-            title:  error.message,
-            text: error.stacktrace,
-            buttons: [
-                {
-                    text: 'B1',
-                    onClick: function() {
-                    }
-                }
-            ]
-        })
+        var navbar = '' +
+            '<div class="navbar top-navbar">' +
+            '<div class="navbar-inner">' +
+            '<a href="#" class="link close-popup close-popup-btn icon-only" style="color: #ff5722;"> Close </a>An error occured' +
+            '</div>' +
+            '</div>';
+
+        var popupHTML = '' +
+            '<div class="popup info-popup tablet-fullscreen">' +
+            '<div class="view navbar-fixed" data-page>' +
+            '<div class="pages">' +
+            '<div data-page class="page">' +
+            navbar +
+            '<div class="page-content">'+
+            '<div class="content-block">' +
+            error.message + '</div>' +
+            '<div class="content-block">' +
+            error.stacktrace +
+            '</div></div></div></div></div></div>';
+
+        slychat.popup(popupHTML);
     }
 };
