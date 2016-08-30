cb8bca8e5ff1b5673f9dbf9263a4eabc1dcff060
Kevin Tardif <kevin@vfpowertech.com>
2016-08-30 15:36:14 -0400

    SessionData.authToken now nullable

--- core/src/main/kotlin/io/slychat/messenger/core/persistence/SessionData.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/persistence/SessionData.kt
@@ -7,5 +7,5 @@ import io.slychat.messenger.core.AuthToken
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class SessionData(
     @param:JsonProperty("authToken")
-    val authToken: AuthToken
+    val authToken: AuthToken?
 )
