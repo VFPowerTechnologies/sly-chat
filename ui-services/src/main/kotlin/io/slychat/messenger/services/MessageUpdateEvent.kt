85386ee2e49e58710ac10fd0cff3151c4f84db38
Kevin Tardif <kevin@vfpowertech.com>
2016-09-09 15:34:01 -0400

    Added MessageUpdateEvent.Expiring/Expired

--- ui-services/src/main/kotlin/io/slychat/messenger/services/MessageUpdateEvent.kt
+++ ui-services/src/main/kotlin/io/slychat/messenger/services/MessageUpdateEvent.kt
@@ -4,4 +4,6 @@ import io.slychat.messenger.core.persistence.ConversationId
 
 sealed class MessageUpdateEvent {
     class Delivered(val conversationId: ConversationId, val messageId: String, val deliveredTimestamp: Long) : MessageUpdateEvent()
+    class Expiring(val conversationId: ConversationId, val messageId: String, val ttl: Long, val expiresAt: Long) : MessageUpdateEvent()
+    class Expired(val conversationId: ConversationId, val messageId: String) : MessageUpdateEvent()
 }
