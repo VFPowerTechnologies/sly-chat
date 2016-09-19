627557697143d0a44dbd22caa2c2bfac57c059e0
Kevin Tardif <kevin@vfpowertech.com>
2016-09-19 11:38:09 -0400

    MessageReadWatcherImpl now handling new messages with isRead=true

--- ui-services/src/main/kotlin/io/slychat/messenger/services/MessageReadWatcherImpl.kt
+++ ui-services/src/main/kotlin/io/slychat/messenger/services/MessageReadWatcherImpl.kt
@@ -1,9 +1,11 @@
 package io.slychat.messenger.services
 
+import io.slychat.messenger.core.persistence.ConversationId
+import io.slychat.messenger.services.messaging.ConversationMessage
 import io.slychat.messenger.services.messaging.MessageService
 import io.slychat.messenger.services.messaging.MessengerService
 import org.slf4j.LoggerFactory
-import rx.Subscription
+import rx.subscriptions.CompositeSubscription
 
 class MessageReadWatcherImpl(
     messageService: MessageService,
@@ -11,18 +13,33 @@ class MessageReadWatcherImpl(
 ) : MessageReadWatcher {
     private val log = LoggerFactory.getLogger(javaClass)
 
-    private var subscription: Subscription? = null
+    private var subscriptions = CompositeSubscription()
 
     init {
-        messageService.messageUpdates.ofType(MessageUpdateEvent.Read::class.java).subscribe { onMessagesRead(it) }
+        subscriptions.add(messageService.messageUpdates.ofType(MessageUpdateEvent.Read::class.java).subscribe { onMessagesRead(it) })
+
+        subscriptions.add(messageService.newMessages.filter {
+            val info = it.conversationMessageInfo.info
+            info.isSent == false && info.isRead == true
+        }.subscribe { onNewReadMessage(it) })
+    }
+
+    private fun onNewReadMessage(conversationMessage: ConversationMessage) {
+        val messageIds = listOf(conversationMessage.conversationMessageInfo.info.id)
+
+        broadcastMessagesRead(conversationMessage.conversationId, messageIds)
     }
 
     private fun onMessagesRead(event: MessageUpdateEvent.Read) {
         if (event.fromSync)
             return
 
-        messengerService.broadcastMessagesRead(event.conversationId, event.messageIds) fail {
-            log.error("Unable to broadcast read messages for {}: {}", event.conversationId, it.message, it)
+        broadcastMessagesRead(event.conversationId, event.messageIds)
+    }
+
+    private fun broadcastMessagesRead(conversationId: ConversationId, messageIds: List<String>) {
+        messengerService.broadcastMessagesRead(conversationId, messageIds) fail {
+            log.error("Unable to broadcast read messages for {}: {}", conversationId, it.message, it)
         }
     }
 
@@ -30,7 +47,6 @@ class MessageReadWatcherImpl(
     }
 
     override fun shutdown() {
-        subscription?.unsubscribe()
-        subscription = null
+        subscriptions.clear()
     }
 }
