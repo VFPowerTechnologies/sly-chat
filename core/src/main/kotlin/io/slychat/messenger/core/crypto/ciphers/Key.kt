0065621260b6581ae7442786739196c42c162545
Kevin Tardif <kevin@vfpowertech.com>
2016-10-04 09:26:02 -0400

    Switched to using JsonDeserializer for Key

--- core/src/main/kotlin/io/slychat/messenger/core/crypto/ciphers/Key.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/crypto/ciphers/Key.kt
@@ -1,10 +1,15 @@
 package io.slychat.messenger.core.crypto.ciphers
 
 import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonToken
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import org.spongycastle.util.encoders.Base64
+import java.io.IOException
 
 class KeySerializer : JsonSerializer<Key>() {
     override fun serialize(value: Key, gen: JsonGenerator, serializers: SerializerProvider) {
@@ -12,10 +17,18 @@ class KeySerializer : JsonSerializer<Key>() {
     }
 }
 
+//jackson won't try and decode the base64 serialization, since it doesn't know it needs to
+//having an alt constructor causes mockitokotlin to try and use for creating dummy values which fails (and I don't
+//wanna have to start peppering createInstance everywhere)
+class KeyDeserializer : JsonDeserializer<Key>() {
+    override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Key {
+        if (p.currentToken != JsonToken.VALUE_STRING)
+            throw IOException("Expected base64 string, got ${p.currentToken}")
+        return Key(p.binaryValue)
+    }
+}
+
 /** Cryptographic key. */
 @JsonSerialize(using = KeySerializer::class)
-class Key(val raw: ByteArray) {
-    //jackson won't try and decode the base64 serialization, since it doesn't know it needs to
-    //this alt constructor does the work without having to create a JsonDeserializer
-    constructor(base64: String) : this(Base64.decode(base64))
-}
+@JsonDeserialize(using = KeyDeserializer::class)
+class Key(val raw: ByteArray)
