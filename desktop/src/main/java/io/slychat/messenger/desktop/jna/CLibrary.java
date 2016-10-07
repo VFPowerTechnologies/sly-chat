92d66208d0445806414705475b03f45ad39930ad
Kevin Tardif <kevin@vfpowertech.com>
2016-10-07 14:44:39 -0400

    Different distribution setup for linux to get around libpackager+jna bug

--- desktop/src/main/java/io/slychat/messenger/desktop/jna/CLibrary.java
+++ desktop/src/main/java/io/slychat/messenger/desktop/jna/CLibrary.java
@@ -4,8 +4,7 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 
 public interface CLibrary extends Library {
-    CLibrary INSTANCE  = (CLibrary) Native.loadLibrary(
-        "c",
+    CLibrary INSTANCE = (CLibrary) Native.loadLibrary(
         CLibrary.class
     );
 
