6f0dffad46b0a57af7ccdde08053b06718b16eeb
Kevin Tardif <kevin@vfpowertech.com>
2016-11-01 11:58:14 -0400

    ClientVersionClientImpl now implements ClientVersionClient

--- core/src/main/kotlin/io/slychat/messenger/core/http/api/versioncheck/ClientVersionClientImpl.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/http/api/versioncheck/ClientVersionClientImpl.kt
@@ -4,9 +4,9 @@ import io.slychat.messenger.core.http.HttpClient
 import io.slychat.messenger.core.http.api.apiGetRequest
 import io.slychat.messenger.core.typeRef
 
-class ClientVersionClientImpl(private val serverBaseUrl: String, private val httpClient: HttpClient) {
+class ClientVersionClientImpl(private val serverBaseUrl: String, private val httpClient: HttpClient) : ClientVersionClient {
     /** Returns true if the client version is up to date, false otherwise. */
-    fun check(version: String): Boolean {
+    override fun check(version: String): Boolean {
         val url = "$serverBaseUrl/v1/client-version/check?v=$version"
 
         return apiGetRequest(httpClient, url, null, emptyList(), typeRef())
