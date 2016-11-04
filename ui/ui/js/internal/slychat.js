7a560c1ad06d46da9cd2178be1538f03c22506b7
Kevin Tardif <kevin@vfpowertech.com>
2016-11-04 09:47:24 -0400

    Added unhandledrejection event listener

--- ui/ui/js/internal/slychat.js
+++ ui/ui/js/internal/slychat.js
@@ -1,3 +1,16 @@
+//this is only supported in more recent versions of webview; doesn't work with
+//the version shipped with jfx
+window.addEventListener('unhandledrejection', function (event) {
+    var stacktrace = event.reason.stacktrace;
+    var stacktraceInfo = '';
+    if (stacktrace)
+        stacktraceInfo = "\n" + stacktrace;
+
+    console.error('Unhandled promise rejection: ' + event.reason + stacktraceInfo);
+
+    event.preventDefault();
+});
+
 window.uiController = new UIController();
 
 uiController.startUI();
