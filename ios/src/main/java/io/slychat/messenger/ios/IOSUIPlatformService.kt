999e1ebb0ba7ce98bdd383866554bbe101c929d1
Kevin Tardif <kevin@vfpowertech.com>
2016-12-07 14:25:13 -0500

    Added ios openURL support

--- ios/src/main/java/io/slychat/messenger/ios/IOSUIPlatformService.kt
+++ ios/src/main/java/io/slychat/messenger/ios/IOSUIPlatformService.kt
@@ -1,8 +1,11 @@
 package io.slychat.messenger.ios
 
+import apple.foundation.NSURL
+import apple.uikit.UIApplication
 import io.slychat.messenger.services.ui.UIPlatformService
 
 class IOSUIPlatformService : UIPlatformService {
     override fun openURL(url: String) {
+        UIApplication.sharedApplication().openURL(NSURL.URLWithString(url))
     }
 }
