9ea9200d7605b699ef37d4ee53ae364c8d736eb0
Kevin Tardif <kevin@vfpowertech.com>
2017-03-08 09:46:58 -0500

    [services] Changed DownloadOperationsImpl retry logic

--- ui-services/src/main/kotlin/io/slychat/messenger/services/files/DownloadOperationsImpl.kt
+++ ui-services/src/main/kotlin/io/slychat/messenger/services/files/DownloadOperationsImpl.kt
@@ -1,11 +1,14 @@
 package io.slychat.messenger.services.files
 
+import io.slychat.messenger.core.UnauthorizedException
 import io.slychat.messenger.core.files.RemoteFile
 import io.slychat.messenger.core.persistence.Download
 import io.slychat.messenger.services.StorageClientFactory
 import io.slychat.messenger.services.auth.AuthTokenManager
 import rx.Observable
 import rx.Scheduler
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class DownloadOperationsImpl(
     private val authTokenManager: AuthTokenManager,
@@ -13,13 +16,29 @@ class DownloadOperationsImpl(
     private val subscribeScheduler: Scheduler
 ) : DownloadOperations {
     override fun download(download: Download, file: RemoteFile): Observable<Long> {
-        //XXX this isn't that great, but getting the retry stuff to work with Observables requires rewriting a bit of
-        //stuff, so do it later
+        //XXX this isn't that great, but getting the retry stuff to work with Observables requires rewriting a bit of stuff
         return Observable.create<Long> { subscriber ->
-            authTokenManager.map {
-                val op = DownloadOperation(it, download, file, storageClientFactory.create(), subscriber)
-                op.run()
-            }.get()
+            val userCredentials = authTokenManager.map { it }.get()
+            val op = DownloadOperation(userCredentials, download, file, storageClientFactory.create(), subscriber)
+            op.run()
+        }.retryWhen {
+            val maxRetries = 3
+            it.zipWith(Observable.range(1, maxRetries + 1), { e, i -> e to i }).flatMap {
+                val (e, i) = it
+
+                if (i > maxRetries)
+                    Observable.error(e)
+                else {
+                    if (e is UnauthorizedException) {
+                        authTokenManager.invalidateToken()
+                        val exp = Math.pow(2.0, i.toDouble())
+                        val secs = Random().nextInt(exp.toInt() + 1).toLong()
+                        Observable.timer(secs, TimeUnit.SECONDS)
+                    }
+                    else
+                        Observable.error(e)
+                }
+            }
         }.subscribeOn(subscribeScheduler)
     }
 }
