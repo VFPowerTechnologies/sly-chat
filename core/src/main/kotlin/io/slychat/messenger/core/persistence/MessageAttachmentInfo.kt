ca3c168872d1221aba5699bac35b944f0f11cc58
Kevin Tardif <kevin@vfpowertech.com>
2017-05-11 08:56:16 -0400

    [core] Added json deserialization support for MessageAttachmentInfo

--- core/src/main/kotlin/io/slychat/messenger/core/persistence/MessageAttachmentInfo.kt
+++ core/src/main/kotlin/io/slychat/messenger/core/persistence/MessageAttachmentInfo.kt
@@ -1,11 +1,20 @@
 package io.slychat.messenger.core.persistence
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class MessageAttachmentInfo(
+    @JsonProperty("n")
     val n: Int,
-    //name as sent by the sender?
+
+    //name as given by the sender
+    @JsonProperty("displayName")
     val displayName: String,
+
     //may be invalid if file was deleted since
+    @JsonProperty("fileId")
     val fileId: String,
+
+    @JsonProperty("isInline")
     //whether or not to request from cache
     val isInline: Boolean
 )
